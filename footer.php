3a3455c4999c347c17bd3eff4e605c50c3ed82b3
Wes Freeman <freeman.wes@gmail.com>
2011-12-11 02:53:17 -0500

    Updated main page to show the custom "abstract" field. Added some code formatting CSS.

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 
-<script src="https://gist.github.com/1455157.js"> </script>
 <script>
+// hack to override the gist default formatting and strip some items
+// I love github, but I think the extra stuff is ugly/distracting
 $(".gist").before("<link>");
     css = $("link:last");
     css.attr({
@@ -8,6 +9,10 @@ $(".gist").before("<link>");
       type: "text/css",
       href: "http://wes.skeweredrook.com/mp-content/themes/wes.skeweredrook.com-theme/css/style.css"
     });
+//$("span.update-time").each(function() { 
+ // var updateTime = new Date(+$(this).text());
+ // $(this).text(updateTime.toString());
+//});
 </script> 
 </div>
 </div>
--- index.php
+++ index.php
@@ -10,12 +10,16 @@ $object_format = array(
 );
 $objects = mp_get_content($object_format);
 foreach($objects as $object) {
-  $this_objects_content = apply_filters('mp_shortcodes', $object['content']);
+  $this_objects_content = apply_filters('mp_shortcodes', $object['custom']['abstract']);
   $this_objects_slug = $mp->get_slug($object['slug_id']);
   $this_objects_type = $object['type'];
+  $date = date_create();
+  date_timestamp_set($date, $object['updated']);
   echo '<article data-updated="'.$object['updated'].'" class="object '.$this_objects_type.'">'."\n";
-  echo '  <h3 class="header"><a href="'.$mp_options['root_url'].$this_objects_slug.'/">'.$object['title'].'</a></h3>'."\n";
+  echo '  <h3 class="header"><a href="'.$mp_options['root_url'].$this_objects_slug.'/">'.$object['title'].'</span></a></h3>'."\n";
+  echo '  <div class="update-time">Posted at <span class="update-time">'.date_format($date, 'Y-m-d H:i:s').'</span> EST</div>';
   echo '  <div class="mp-content" data-slug="'.$this_objects_slug.'">'.$this_objects_content.'</div>'."\n";
+  echo '  <a href="'.$mp_options['root_url'].$this_objects_slug.'/">Read more...</a>';
   echo '</article>'."\n";
 }
 get_template_part("footer");
